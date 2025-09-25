e527712d22c87680228f25d0e66b1ab4537eb4b3
yasin-wajihaa <wajihaazafar@gmail.com>
2025-09-25 20:48:19 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+print(__name__)
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def heyya():
+    return "heyya!"
+
+if __name__ == "__main__" :
+    app.run(host="0.0.0.0" , debug=True)
+
