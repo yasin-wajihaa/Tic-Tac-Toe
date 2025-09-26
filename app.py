e152dceb5aceaabfd4d1cccd072ea33b5b73a738
yasin-wajihaa <wajihaazafar@gmail.com>
2025-09-26 14:43:56 +0530

    added css and requirement files

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 print(__name__)
 
-from flask import Flask
+from flask import Flask ,render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def heyya():
-    return "heyya!"
+    return render_template("home.html")
 
 if __name__ == "__main__" :
     app.run(host="0.0.0.0" , debug=True)
